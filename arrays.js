f5a9bb535807a16f9ea348ed6e715660927c3e36
Mohian Dipta <mohianmostofa@gmail.com>
2021-05-04 14:26:30 +0600

    array done

--- arrays.js
+++ arrays.js
@@ -45,3 +45,50 @@ const sortLang = language.sort(function name(a, b) {
 });
 
 console.log(sortLang);
+
+
+
+// filtering an array
+const randomNum = [1, -2, 2, -1, -4, 3, 4, -3]
+
+randomNum.sort();
+const filterPositive = randomNum.filter(function (value) {
+    return value >= 0;
+});
+const filterNegative = randomNum.filter(function (value) {
+    return value <= 0;
+});
+
+console.log(randomNum);
+console.log(filterNegative);
+console.log(filterPositive);
+
+
+
+// mapping an array
+const digit = [1, 2, -5, 3, -1, 4, 5, 'open']
+
+const filtered = digit.filter(function (value) {
+    return value >= 0;
+});
+
+const items = filtered.map(n => '<li>' + n + '</li>');
+const html = '<ul>' + items.join('') + '</ul>';
+
+console.log(html);
+
+
+
+// Exercise-1
+// Array from range
+
+const rangeFromArray = arrayFromRange(1, 8);
+
+function arrayFromRange(min, max) {
+    const output = [];
+    for (let i = min; i <= max; i++) {
+        output.push(i);
+        return output;
+    }
+}
+console.log(rangeFromArray);
